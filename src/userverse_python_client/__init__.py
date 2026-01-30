c9877c70929109d6f7ef13b306fe2abad1258304
Skhendle@gmail.com <skhendle@gmail.com>
2026-01-30 15:42:56 +0200

    expose UverseCompanyUserRolesManagement

--- src/userverse_python_client/__init__.py
+++ src/userverse_python_client/__init__.py
@@ -1,13 +1,13 @@
 """userverse_python_client public API."""
 
-from typing import TYPE_CHECKING, Any
-
 from .clients.user import UverseUserClient
 from .clients.company import UverseCompanyClient
 from .clients.company_user_management import UverseCompanyUserManagementClient
+from .clients.company_user_roles_management import UverseCompanyUserRolesManagement
 
 __all__ = [
     "UverseUserClient",
     "UverseCompanyClient",
     "UverseCompanyUserManagementClient",
+    "UverseCompanyUserRolesManagement",
 ]
